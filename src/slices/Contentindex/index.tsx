b706ba0a54e514bc66987998c72a13e075c73980
Yansi Viramgama <yansi.v@shaligraminfotech.com>
2024-05-06 13:46:09 +0530

    changes

---
+++ src/slices/Contentindex/index.tsx
@@ -0,0 +1,33 @@
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import ContentList from "./ContentList";
+/**
+ * Props for `Contentindex`.
+ */
+export type ContentindexProps = SliceComponentProps<Content.ContentindexSlice>;
+
+/**
+ * Component for "Contentindex" Slices.
+ */
+const Contentindex = ({ slice }: ContentindexProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <Heading size="lg" className="mb-8">
+      {slice.primary.heading}
+      </Heading>
+      {isFilled.richText(slice.primary.description)&&(
+        <div className="prose prose-xl prose-invert mb-10">
+          <PrismicRichText field={slice.primary.description}/>
+        </div>
+      )}
+      <ContentList/>
+    </Bounded>
+  );
+};
+
+export default Contentindex;
